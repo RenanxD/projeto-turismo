c6b344df3bc16d96967069efa12b235adf4ddae1
Renan de Paula da Silva <renan.silva@unibqe.lan>
2024-07-02 17:25:57 -0300

    ajustando botao de voltar nas paginas

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -4,9 +4,11 @@
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <div class="flex items-center">
                     <button onclick="window.history.back()" class="btn-back botao-voltar">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-                        </svg>
+                        <div class="circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 arrow-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+                            </svg>
+                        </div>
                     </button>
                     <h2 class="font-semibold text-xl text-gray-800 leading-tight ml-6">
                         @yield('title')
